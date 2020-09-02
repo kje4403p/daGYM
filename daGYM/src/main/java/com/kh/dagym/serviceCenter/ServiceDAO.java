0153cf35dd037b8c7e1e9a2b87d8d94fb59df7e1
kimjo <kimjo@DESKTOP-212KL0S>
2020-09-02 17:40:44 +0900

    0902오후 저장

--- daGYM/src/main/java/com/kh/dagym/serviceCenter/ServiceDAO.java
+++ daGYM/src/main/java/com/kh/dagym/serviceCenter/ServiceDAO.java
@@ -1,8 +1,14 @@
 package com.kh.dagym.serviceCenter;
 
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.kh.dagym.common.Board;
+import com.kh.dagym.common.PageInfo;
 
 @Repository
 public class ServiceDAO {
@@ -13,6 +19,32 @@ public class ServiceDAO {
 	
 	public int getListCount(int type) {
 		return sqlSession.selectOne("serviceMapper.getListCount",type);
+	}
+
+	public List<Board> selectFaqList(PageInfo pInfo) {
+		
+		int offset=(pInfo.getCurrentPage()-1) *pInfo.getLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, pInfo.getLimit());
+		
+		return sqlSession.selectList("serviceMapper.selectFaqList", pInfo.getBoardType(),rowBounds);
+	}
+
+	
+	/** faq 게시글 상세조회 DAO
+	 * @param boardNo
+	 * @return board
+	 */
+	public Board selectFaqBoard(int boardNo) {
+		return sqlSession.selectOne("serviceMapper.selectFaqBoard", boardNo);
+	}
+
+	/** 조회수 증가 DAO
+	 * @param boardNo
+	 * @return result
+	 */
+	public int increaseCount(int boardNo) {
+		return sqlSession.update("serviceMapper.increaseCount",boardNo);
 	}
 
 }
--- daGYM/src/main/java/com/kh/dagym/serviceCenter/service/ServiceBoardImpl.java
+++ daGYM/src/main/java/com/kh/dagym/serviceCenter/service/ServiceBoardImpl.java
@@ -2,9 +2,12 @@ package com.kh.dagym.serviceCenter.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.dagym.common.Board;
 import com.kh.dagym.common.PageInfo;
 import com.kh.dagym.serviceCenter.ServiceDAO;
 
@@ -22,9 +25,37 @@ public class ServiceBoardImpl implements ServiceBoard{
 	@Override
 	public PageInfo pagination(int type, int cp) {
 		
+		//전체 게시글 수 조회
 		int listCount = serviceDAO.getListCount(type);
 				
-		return null;
+		pInfo.setPageInfo(cp, listCount, type);
+	
+		return pInfo;
+	}
+
+	//faq 리스트 조회 service구현
+	@Override
+	public List<Board> selectFaqList(PageInfo pInfo) {
+		
+		return serviceDAO.selectFaqList(pInfo);
+	}
+
+	//faq 게시글 상세조회 Service구현
+	
+	@Override
+	public Board selectFaqBoard(int boardNo) {
+		Board board = serviceDAO.selectFaqBoard(boardNo);
+		
+		if(board != null) {
+			int result = serviceDAO.increaseCount(boardNo);
+			
+			if(result>0) {
+				board.setViews(board.getViews()+1);
+			}
+		}
+		
+		
+		return board;
 	}
 	
 	
