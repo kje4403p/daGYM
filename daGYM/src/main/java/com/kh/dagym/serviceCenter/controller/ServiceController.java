38d66c9b8c3507b0035ca053546e2e8c841b86da
kimjo <kimjo@DESKTOP-212KL0S>
2020-09-08 17:35:08 +0900

    09-08 faq게시판 완료

--- daGYM/src/main/java/com/kh/dagym/serviceCenter/controller/ServiceController.java
+++ daGYM/src/main/java/com/kh/dagym/serviceCenter/controller/ServiceController.java
@@ -1,11 +1,14 @@
 package com.kh.dagym.serviceCenter.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +16,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+import com.kh.dagym.common.Attachment;
 import com.kh.dagym.common.Board;
 import com.kh.dagym.common.PageInfo;
 import com.kh.dagym.member.model.vo.Member;
-import com.kh.dagym.serviceCenter.Search;
 import com.kh.dagym.serviceCenter.service.ServiceBoard;
+import com.kh.dagym.serviceCenter.vo.Search;
 
 @SessionAttributes({"loginMember"})
 @Controller
@@ -58,7 +63,8 @@ public class ServiceController {
 		model.addAttribute("pInfo", pInfo);
 		model.addAttribute("nList", nList);
 		model.addAttribute("mList", mList);
-		
+		boolean flag = true;
+		model.addAttribute("flag", flag);
 		return "serviceCenter/faq";
 	}
 	
@@ -70,13 +76,39 @@ public class ServiceController {
 						   RedirectAttributes rdAttr,HttpServletRequest request) {
 		System.out.println("확인");
 		Board board = serviceBoard.selectFaqBoard(boardNo);
+		Map<String,Object > map = new HashMap<String, Object>();
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
 		
 		String url = null;
 		if(board != null) {
-			Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
-			return gson.toJson(board);
+			
+			List<Attachment> files = serviceBoard.selectFaqFiles(boardNo);
+			
+			
+			if(!files.isEmpty()) {
+//				int i=0;
+//				for(Attachment at : files) {
+//					if(at != null) {
+//						map.put("file"+i+"", at);
+//						System.out.println(at.getFileLevel());
+//						i++;
+//					}
+//					
+//				}
+			map.put("board", board);
+			map.put("files", files);
+//			map.put("file1", files.get(1));
+//			map.put("file2", files.get(2));
+//			map.put("file3", files.get(3));
+			
+		}else {
+			map.put("board", board);
+			
 		}
-		return null;
+	}
+		
+		return gson.toJson(map);
+		
 	}
 	
 	//게시글 검색
@@ -84,12 +116,161 @@ public class ServiceController {
 	public String search(@PathVariable int type,
 			@RequestParam(value="cp",required=false,defaultValue="1")int cp,
 			Search search, Model model){
-		System.out.println(type);
-		System.out.println(search);
+	
 		
 		PageInfo pInfo = serviceBoard.pagination(type, cp,search);
+		
+		List<Board> boardList = serviceBoard.selectSearchList(pInfo,search);
+		for(Board b:boardList) {
+			System.out.println(b);
+		}
+		List<Board> nList = serviceBoard.selectBoardNo(pInfo);
+		List<com.kh.dagym.admin.model.vo.Member> mList = serviceBoard.selectMemberId(pInfo);
+		model.addAttribute("bList",boardList);
+		model.addAttribute("pInfo", pInfo);
+		model.addAttribute("nList", nList);
+		model.addAttribute("mList", mList);
+		boolean flag = false;
+		model.addAttribute("flag", flag);
+		boolean flag2=true;
+		model.addAttribute("flag2", flag2);
+		
 	
-		return null;
+		return "serviceCenter/faq";
 	}
-
+	
+	@RequestMapping("{type}/insert")
+	public String insertView() {
+		return "serviceCenter/faqInsert";
+	}
+	
+	//게시글 등록
+	@Transactional(rollbackFor = Exception.class)
+	@RequestMapping(value="{type}/insertAction",method=RequestMethod.POST)
+	public String insertAction(@PathVariable int type, Board board,
+			Model model,HttpServletRequest request, RedirectAttributes rdAttr,
+			@RequestParam(value="thumbnail",required = false) MultipartFile thumbnail,
+			@RequestParam(value="images", required=false)List<MultipartFile>images) {
+		
+		Member loginMember=(Member) model.getAttribute("loginMember");
+		
+		board.setBoardType(type);
+		board.setBoardWriter(loginMember.getMemberNo()+"");
+		
+		images.add(0, thumbnail);
+		
+		String savePath = request.getSession().getServletContext().getRealPath("resources/uploadImages");
+		
+		
+		int result = serviceBoard.insertFaq(board,images,savePath);
+		String status=null;
+		String text=null;
+		String msg=null;
+		String url=null;
+		
+		if(result>0) {
+			status="success";
+			text="게시글 삽입성공";
+			msg="게시글 등록 성공";
+			//url="board/"+type+"/"+board.getBoardNo()+"?cp=1";
+			url="../faq/"+board.getBoardType()+"?cp=1";
+		} else {
+			status="error";
+			text="게시글 등록 실패";
+			url="faqInsert";
+		}
+		rdAttr.addFlashAttribute("status", status);
+		rdAttr.addFlashAttribute("msg", msg);
+		
+		return "redirect:"+url;
+	}
+	
+	//faq게시글 수정
+	@RequestMapping("faq/{type}/{boardNo}/update")
+	public ModelAndView faqUpdate(@PathVariable int boardNo,ModelAndView mv) {
+		
+		Board board = serviceBoard.selectFaqBoard(boardNo);
+		
+		if(board != null) {
+			List<Attachment> files = serviceBoard.selectFaqFiles(boardNo);
+			
+			for(Attachment at : files) {
+				System.out.println(at);
+			}
+			mv.addObject("files", files);
+		}
+		
+		mv.addObject("board", board);
+		mv.setViewName("serviceCenter/faqUpdate");
+	return mv;	
+	}
+	
+	//faq게시글 수정
+	@RequestMapping("faq/{type}/{boardNo}/updateAction")
+	public String faqUpdateAction(@PathVariable int type,
+										@PathVariable int boardNo,
+										ModelAndView mv,
+										Board upBoard, int cp,
+										 RedirectAttributes rdAttr,
+										 HttpServletRequest request,
+										 @RequestParam (value="thumbnail", required=false)MultipartFile thumbnail,
+										 @RequestParam (value="images", required=false)List<MultipartFile> images,
+										 boolean[] deleteImages) {
+		
+		upBoard.setBoardNo(boardNo);
+		
+		images.add(0, thumbnail);
+		
+		String savePath = request.getSession().getServletContext().getRealPath("resources/uploadImages");
+		
+		int result = serviceBoard.updateFaqBoard(upBoard,savePath,images,deleteImages);
+		
+		String status=null;
+		String text=null;
+		String msg=null;
+		String url=null;
+		if(result>0) {
+			status="success";
+			msg="게시글 수정성공";
+			url = "../../"+type+"?cp="+cp;
+			rdAttr.addFlashAttribute("status",status);
+			rdAttr.addFlashAttribute("msg",msg);
+			
+		}else {
+			status="error";
+			msg="게시글 수정 실패";
+			url=request.getHeader("referer");
+		}
+		
+		return "redirect:"+url;
+	}
+	
+	@RequestMapping("faq/{type}/{boardNo}/delete")
+	public String delectFaqBoard(@PathVariable int type,
+			  					@PathVariable int boardNo,
+			  					RedirectAttributes rdAttr,
+								 HttpServletRequest request) {
+		int result = serviceBoard.deleteFaqBoard(boardNo);
+		
+		String status=null;
+		String text=null;
+		String msg=null;
+		String url=null;
+		
+		if(result>0) {
+			status="success";
+			msg="삭제성공";   
+			url = "../../"+type;
+			
+		}else {
+			status="error";
+			msg="삭제실패";
+			url = request.getHeader("referer");
+		}
+		rdAttr.addFlashAttribute("status", status);
+		rdAttr.addFlashAttribute("msg", msg);
+		return "redirect:"+url; 
+	}
+	
+	
 }
--- daGYM/src/main/java/com/kh/dagym/serviceCenter/service/ServiceBoardImpl.java
+++ daGYM/src/main/java/com/kh/dagym/serviceCenter/service/ServiceBoardImpl.java
@@ -2,18 +2,24 @@ package com.kh.dagym.serviceCenter.service;
 
 
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.dagym.admin.model.vo.Member;
+import com.kh.dagym.common.Attachment;
 import com.kh.dagym.common.Board;
 import com.kh.dagym.common.PageInfo;
-import com.kh.dagym.serviceCenter.Search;
 import com.kh.dagym.serviceCenter.ServiceDAO;
+import com.kh.dagym.serviceCenter.vo.Search;
 
 
 
@@ -49,15 +55,17 @@ public class ServiceBoardImpl implements ServiceBoard{
 	
 	@Override
 	public Board selectFaqBoard(int boardNo) {
+		
 		Board board = serviceDAO.selectFaqBoard(boardNo);
-		System.out.println(board.toString());
+		
 		if(board != null) {
 			
 			
 			int result = serviceDAO.increaseCount(boardNo);
-			
+			System.out.println(board.getViews());
 			if(result>0) {
 				board.setViews(board.getViews()+1);
+				
 			}
 		}
 		
@@ -75,7 +83,9 @@ public class ServiceBoardImpl implements ServiceBoard{
 		
 		int searchListCount = serviceDAO.getSearchListCount(map);
 		
-		return null;
+		pInfo.setPageInfo(cp, searchListCount, type);
+		
+		return pInfo;
 	}
 
 	// faq보드넘버 조회 service구현
@@ -91,9 +101,201 @@ public class ServiceBoardImpl implements ServiceBoard{
 		
 		return serviceDAO.selectFaqMemberIdList(pInfo);
 	}
+
+
+	//faq 검색 목록 조회 Service구현
+	@Override
+	public List<Board> selectSearchList(PageInfo pInfo, Search search) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("search", search);
+		map.put("type", pInfo.getBoardType());
+		return serviceDAO.selectSearchList(pInfo,map);
+	}
+
+
+	//faq 글 등록 Service 구현
+	@Transactional(rollbackFor = Exception.class)
+	@Override
+	public int insertFaq(Board board, List<MultipartFile> images, String savePath) {
+
+		int result =0;
+		
+		int boardNo = serviceDAO.selectNextNo(board.getBoardType());
+		
+		if(boardNo>0) {
+			
+			board.setBoardNo(boardNo);
+			
+			board.setBoardContent(replaceParameter(board.getBoardContent()));
+			
+			result = serviceDAO.insertFaq(board);
+			
+			List<Attachment> files = new ArrayList<Attachment>();
+			
+			Attachment at = null;
+			
+			String filePath = "/resources/uploadImages";
+			
+			for(int i=0; i<images.size(); i++) {
+				
+				if(!images.get(i).getOriginalFilename().equals("")) {
+					
+					String changeName = rename(images.get(i).getOriginalFilename());
+					
+					at = new Attachment(boardNo,images.get(i).getOriginalFilename(),
+							changeName, filePath, i);
+					
+					result = serviceDAO.insertFaqAttachment(at);
+				}
+				
+				files.add(at);
+					
+			}
+			
+			if(result>0) {
+				
+				for(int i=0; i<images.size(); i++) {
+					
+					if(!images.get(i).getOriginalFilename().equals("")) {
+						
+						try {
+							images.get(i).transferTo(new File(savePath+"/"+files.get(i).getFileChangeName()));
+						} catch (Exception e) {
+							e.printStackTrace();
+							
+							serviceDAO.deleteFaqAttachment(boardNo);
+						}
+					}
+				}
+			}
+			
+		}
+		return result;
+	}
 	
 	
 	
+	private String replaceParameter(String param) {
+        String result = param;
+        if(param != null) {
+            result = result.replaceAll("&", "&amp;");
+            result = result.replaceAll("<", "&lt;");
+            result = result.replaceAll(">", "&gt;");
+            result = result.replaceAll("\"", "&quot;");
+        }
+
+        return result;
+    }
 	
+	 public String rename(String originFileName) {
+	        SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmss");
+	        String date = sdf.format(new java.util.Date(System.currentTimeMillis()));
+
+	        int ranNum = (int)(Math.random()*100000); // 5자리 랜덤 숫자 생성
+
+	        String str = "_" + String.format("%05d", ranNum);
+	        //String.format : 문자열을 지정된 패턴의 형식으로 변경하는 메소드
+	        // %05d : 오른쪽 정렬된 십진 정수(d) 5자리(5)형태로 변경. 빈자리는 0으로 채움(0)
+
+	        String ext = originFileName.substring(originFileName.lastIndexOf("."));
+
+	        return date + "" + str + ext;
+	    }
+
+
+	 //faq 이미지 조회 service 구현
+	@Override
+	public List<Attachment> selectFaqFiles(int boardNo) {
+
+		return serviceDAO.selectFaqFiles(boardNo);
+	}
+
+	//게시글 업데이트 Service 구현
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public int updateFaqBoard(Board upBoard, String savePath, List<MultipartFile> images, boolean[] deleteImages) {
+		
+		upBoard.setBoardContent(replaceParameter(upBoard.getBoardContent()));
+		
+		int result = serviceDAO.updateFaqBoard(upBoard);
+		
+		if(result>0) {
+			List<Attachment> oldFiles = serviceDAO.selectFaqFiles(upBoard.getBoardNo());
+			
+			String filePath = "/resources/uploadImages";
+			
+			List<Attachment> files = new ArrayList<Attachment>();
+			List<Attachment> removeFiles = new ArrayList<Attachment>();
+			Attachment at = null;
+			
+			for(int i=0; i<images.size(); i++) {
+				if(!images.get(i).getOriginalFilename().equals("")) {
+					
+					String changeFileName = rename(images.get(i).getOriginalFilename());
+					
+					at = new Attachment(upBoard.getBoardNo(), 
+							images.get(i).getOriginalFilename(), 
+							changeFileName, filePath, i);
+					
+					boolean flag = false;
+					for(Attachment old: oldFiles) {
+						if(old.getFileLevel()==i) {
+							flag = true;
+							removeFiles.add(old);
+							
+							at.setFileNo(old.getFileNo());
+							
+							break;
+							
+						}
+					}
+					
+					if(flag) {
+						result = serviceDAO.updateFaqAttachment(at);
+					}else {
+						result = serviceDAO.insertFaqAttachment(at);
+					}
+					
+				}else {
+					if(deleteImages[i]) {
+						for(Attachment old:oldFiles) {
+							
+							if(old.getFileLevel() ==i) {
+								result = serviceDAO.deleteFaqAttachment2(old.getFileNo());
+								removeFiles.add(old);
+							}
+						}
+					}
+				}
+				files.add(at);
+			}
+			
+			if(result>0) {
+				for(int i=0; i<images.size(); i++) {
+					if(!images.get(i).getOriginalFilename().equals("")) {
+						try {
+							images.get(i).transferTo(new File(savePath+"/"+files.get(i).getFileChangeName()));
+							
+						} catch (Exception e) {
+
+							serviceDAO.deleteFaqAttachment(upBoard.getBoardNo());
+						}
+					}
+				}
+			}
+			for(Attachment removeFile : removeFiles) {
+				File rm = new File(savePath + "/" + removeFile.getFileChangeName());
+				rm.delete();
+			}
+			
+		}
+		return result;
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	@Override
+	public int deleteFaqBoard(int boardNo) {
+		return serviceDAO.deleteFaqBoard(boardNo);
+	}
 
 }
